8f7368aa9b7533f4e71ef8393f50fa16c26789c7
Jake O'Donnell <jakeod99@gmail.com>
2019-06-19 16:39:50 -0400

    checks cap state software for name

--- lib/app/modules/del/confirm_capability_statement_sequence.rb
+++ lib/app/modules/del/confirm_capability_statement_sequence.rb
@@ -31,7 +31,7 @@ module Inferno
                 metadata{
                     id '02'
                     desc %(
-                        Tests if the Capability statement has exactly 1, valid status (active, retired, draft, or unknown)
+                        Tests if the capability statement has exactly 1, valid status (active, retired, draft, or unknown)
                     )
                 }
 
@@ -46,16 +46,43 @@ module Inferno
                 metadata{
                     id '03'
                     desc %(
-                        Tests if the Capability statement has exactly 1, valid date
+                        Tests if the capability statement has exactly 1, valid date
                     )
                 }
 
                 #Below Regex is heavily taken from hl7 with some improvements
                 assert !@cap.date.nil?, "Capability statement date is nil, must have exactly 1 valid date"
-                assert conforms_to_dateTime_format(@cap.date), "Capability status is " + @cap.date + ", not a valid dateTime (see https://www.hl7.org/fhir/datatypes.html)"
+                assert conforms_to_dateTime_format(@cap.date), "Capability status is " + @cap.date.inspect + ", not a valid dateTime (see https://www.hl7.org/fhir/datatypes.html)"
 
             end
 
+            test 'Capability Statement has valid kind' do
+
+                metadata{
+                    id '04'
+                    desc %(
+                        Tests if the capability statement has exactly 1, valid kind (instance, capability, requirements)
+                    )
+                }
+
+                validKindCodes = ["instance", "capability", "requirements"]
+                assert !@cap.kind.nil?, "Capability statement kind is nil, must have exactly 1 valid kind"
+                assert validKindCodes.include?(@cap.kind), "Capability status is " + @cap.kind.inspect + ", not a valid code"
+
+            end
+
+            test 'Capability Statement has valid software (if applicable)' do
+
+                metadata{
+                    id '05'
+                    desc %(
+                        If the capability statement has a software section, this checks that it is valid
+                    )
+                }
+
+                assert @cap.software.nil? || !@cap.software.name.empty?(), "If the capability statement has a software entity, the software entity must have a name"
+
+            end
 
         end
     end 
